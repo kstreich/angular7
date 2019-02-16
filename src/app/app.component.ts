7ce278992adb424deb90b124d130ac4990b2c31f
Kim Streicher <kim.streicher@allata.com>
2019-02-16 16:05:30 -0700

    nav stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //unigue name which allows for reference in other areas of the app
+  templateUrl: './app.component.html', //specifies the html file
+  styleUrls: ['./app.component.scss'] // sass or css file
 })
+
+//all of the logic JS goes here
 export class AppComponent {
   title = 'ng7';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
